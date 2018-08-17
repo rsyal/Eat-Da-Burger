15ad7804f6403aabdedad4ca6942092ba24a3621
rsyal <35440553+rsyal@users.noreply.github.com>
2018-08-16 19:09:40 -0500

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Jalapeno', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Avocado Bean', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Ranch with BBQ Pork', FALSE);
